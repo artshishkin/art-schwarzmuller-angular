7e573b9d430e9eb7d84621372d688edeb46f44d4
Art <d.art.shishkin@gmail.com>
2021-10-01 08:50:45 +0300

    22. Working with Component Styles (#2)

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,14 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: ['h3 {color: aqua}',
+      `
+      h3 {
+        background-color: black;
+        padding: 20px;
+      }`
+  ]
 })
 export class AppComponent {
 }
