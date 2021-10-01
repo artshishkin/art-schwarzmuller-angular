57eb0c02919fa7217c7b8a24c3baa9cc1a1eef07
Art <d.art.shishkin@gmail.com>
2021-10-01 10:43:38 +0300

    27. Property Binding (#2)

--- my-first-app/src/app/components/servers/servers.component.ts
+++ my-first-app/src/app/components/servers/servers.component.ts
@@ -7,7 +7,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
+  allowNewServer: boolean = false;
+
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
   }
 
   ngOnInit(): void {
