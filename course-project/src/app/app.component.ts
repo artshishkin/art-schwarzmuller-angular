87fc5b16f63d4b9c700ced90079ef0bce4c84765
Art <d.art.shishkin@gmail.com>
2021-10-04 17:05:24 +0300

    85.1. Adding Navigation with Event Binding and ngIf - My Implementation (#5 Section 6: Course Project - Components & Databinding)

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -7,4 +7,10 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project';
+
+  pageToShow: string = 'recipes';
+
+  onNavigationChanged(page: string) {
+    this.pageToShow = page;
+  }
 }
