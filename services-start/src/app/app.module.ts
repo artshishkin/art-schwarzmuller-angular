3e3dcb60471d3b38419521cb2ae015b09a7004f1
Art <d.art.shishkin@gmail.com>
2021-10-06 11:56:25 +0300

    113. Services in Angular 6+ (#8)

--- services-start/src/app/app.module.ts
+++ services-start/src/app/app.module.ts
@@ -6,8 +6,6 @@ import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {AccountComponent} from './account/account.component';
 import {NewAccountComponent} from './new-account/new-account.component';
-import {AccountsService} from "./shared/accounts.service";
-import {LoggingService} from "./shared/logging.service";
 
 @NgModule({
   declarations: [
@@ -19,7 +17,7 @@ import {LoggingService} from "./shared/logging.service";
     BrowserModule,
     FormsModule,
   ],
-  providers: [LoggingService, AccountsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
