38f5ecf74c0ce08bd8516acc78bbbe29b3012d77
Art <d.art.shishkin@gmail.com>
2021-10-06 14:43:12 +0300

    Assignment 5: Practicing Services - Counter Service (#8)

---
+++ services-assignment-start/src/app/services/counter.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+
+  activeToInactiveCounter: number = 0;
+  inactiveToActiveCounter: number = 0;
+
+  constructor() {
+  }
+
+  get totalTransitions(): number {
+    console.log(`CounterService totalTransitions getter is invoked: ${this.inactiveToActiveCounter} + ${this.activeToInactiveCounter}`);
+    return this.activeToInactiveCounter + this.inactiveToActiveCounter;
+  }
+}
--- services-assignment-start/src/app/services/users.service.ts
+++ services-assignment-start/src/app/services/users.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {CounterService} from "./counter.service";
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,19 @@ export class UsersService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
-  constructor() { }
+  constructor(private counterService: CounterService) {
+  }
 
   moveToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.activeToInactiveCounter++;
   }
 
   moveToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterService.inactiveToActiveCounter++;
   }
 
 }
