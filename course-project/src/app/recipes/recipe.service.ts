3591c320ba570c5242e8c7cff0829a71ea172e6e
Art <d.art.shishkin@gmail.com>
2021-10-06 18:55:30 +0300

    121. Adding Ingredients to Recipes (#9)

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,12 +1,24 @@
 import {EventEmitter} from "@angular/core";
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe("The Test Recipe 1", "This is just a test 1", 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe("The Test Recipe 2", "This is just a test 2", 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkf0Oj33wT9HsUqExwLAyh59ekSNb4BP272Q&usqp=CAU'),
-    new Recipe("The Test Recipe 3", "This is just a test 3", 'fake')
+    new Recipe("The Test Recipe 1", "This is just a test 1", 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [
+        new Ingredient('Carrot', 12),
+        new Ingredient('Potato', 3),
+        {name: 'Meat', amount: 2},
+      ]),
+    new Recipe("The Test Recipe 2", "This is just a test 2", 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkf0Oj33wT9HsUqExwLAyh59ekSNb4BP272Q&usqp=CAU',
+      [
+        {name: 'Bread', amount: 2},
+        {name: 'Butter', amount: 30},
+        {name: 'Salt', amount: 123},
+      ]
+    ),
+    new Recipe("The Test Recipe 3", "This is just a test 3", 'fake', [])
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
