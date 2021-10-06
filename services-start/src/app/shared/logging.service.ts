10fd9fe343be6b037a185cb7a3e9e443326aae48
Art <d.art.shishkin@gmail.com>
2021-10-06 10:17:34 +0300

    107.2. Injecting the Logging Service into Components - provide one by component, not a Singleton (#8)

--- services-start/src/app/shared/logging.service.ts
+++ services-start/src/app/shared/logging.service.ts
@@ -1,5 +1,12 @@
 export class LoggingService {
 
+  private serviceId: number;
+
+  constructor() {
+    this.serviceId = Math.floor(Math.random() * 1_000_000);
+    console.log(`Created LoggingService with id: ${this.serviceId}`);
+  }
+
   logStatusChange(status: string) {
     console.log('A server status changed, new status: ' + status);
   }
