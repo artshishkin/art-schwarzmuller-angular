85590e0e8d6c80a2b008806c9bfc565c0773d87f
Art <d.art.shishkin@gmail.com>
2021-10-07 15:19:48 +0300

    140. Using Query Parameters - Practice (#10)

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -12,20 +12,21 @@ export class EditServerComponent implements OnInit {
   server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit: boolean = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    console.log("EditServerComponent: ngOnInit");
     const id: number = +this.route.snapshot.params['id'];
-    const allowEdit: boolean = this.route.snapshot.queryParams['allowEdit'];
-    const fragment = this.route.snapshot.fragment;
-    console.log(`Server ID: ${id}. Allow Edit - ${allowEdit}. Fragment: ${fragment}`);
+    // this.allowEdit = this.route.snapshot.queryParams['allowEdit'];
+    // const fragment = this.route.snapshot.fragment;
+    // console.log(`Server ID: ${id}. Allow Edit - ${this.allowEdit}. Fragment: ${fragment}`);
 
     this.route.queryParams.subscribe((queryParams: Params) => {
       console.log(`Query Params in Subscription: ${JSON.stringify(queryParams)}`);
+      this.allowEdit = queryParams['allowEdit'] === 'true';
     }); //no need to unsubscribe - this is Angular build-in Observable and Angular will handle it for us
 
     this.route.fragment.subscribe(fr => console.log(`Fragment in Subscription: ${fr}`));
