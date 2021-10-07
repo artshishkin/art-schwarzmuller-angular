18ae9d7af5bc74dfe85b821aec463abff186c071
Art <d.art.shishkin@gmail.com>
2021-10-07 23:15:31 +0300

    150. Passing Static Data to a Route (#10)

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import {UserComponent} from "./users/user/user.component";
 import {ServersComponent} from "./servers/servers.component";
 import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {NgModule} from "@angular/core";
 import {AuthGuard} from "./auth-guard.service";
 import {CanDeactivateGuard} from "./can-deactivate.guard";
+import {ErrorPageComponent} from "./error-page/error-page.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -28,7 +28,8 @@ const routes: Routes = [
       {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]},
     ]
   },
-  {path: 'not-found', component: PageNotFoundComponent},
+  // {path: 'not-found', component: PageNotFoundComponent},
+  {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
   {path: '**', redirectTo: '/not-found'},
 ];
 
