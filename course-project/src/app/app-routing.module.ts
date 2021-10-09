134a931f943354eca1d222952b6ccb6396bb36fd
Art <d.art.shishkin@gmail.com>
2021-10-09 11:32:44 +0300

    159. Child Routes: Challenge - my solution (#11)

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {RecipesComponent} from "./recipes/recipes.component";
+import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
 
 const routes: Routes = [
   {path: 'shopping-list', component: ShoppingListComponent},
-  {path: 'recipes', component: RecipesComponent},
+  {
+    path: 'recipes', component: RecipesComponent, children: [
+      {path: ':id/view', component: RecipeDetailComponent}
+    ]
+  },
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: '**', redirectTo: '/recipes'},
 ];
--- course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,12 +10,18 @@ import {RecipeService} from "../recipe.service";
 })
 export class RecipeDetailComponent implements OnInit, OnChanges {
 
-  @Input() recipe: Recipe;
+  recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+
+    this.route.params.subscribe((params:Params)=>{
+      const recipeId:number = +params['id'];
+      this.recipe = this.recipeService.getRecipes()[recipeId];
+    })
   }
 
   ngOnChanges(changes: SimpleChanges): void {
