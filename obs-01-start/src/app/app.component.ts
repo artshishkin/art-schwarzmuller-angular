ada0400d3f74b901543813479e9da5e20b5b4767
Art <d.art.shishkin@gmail.com>
2021-10-10 12:09:25 +0300

    176.1. Subjects - EventEmitter (old approach) (#12)

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from "./user/user.service";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  userActivated: boolean = false;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe(activated => this.userActivated = activated);
+  }
 }
