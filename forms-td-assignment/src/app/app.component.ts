48f3fe1fd7e5dd05031607689a0d16d54e1beb30
Art <d.art.shishkin@gmail.com>
2021-10-11 09:58:29 +0300

    Assignment 6: Practicing Template-Driven Forms - my solution (#14)

--- forms-td-assignment/src/app/app.component.ts
+++ forms-td-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  subscriptions: string[] = ["Basic", "Advanced", "Pro"];
+
+  @ViewChild('f') subscriptionForm: NgForm;
+
+  onSubmit(formContent: any) {
+    console.log(formContent);
+    this.subscriptionForm.reset();
+  }
 }
