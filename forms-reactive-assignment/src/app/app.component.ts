2a77ef189125a6d623ffdf182c44067366163013
Art <d.art.shishkin@gmail.com>
2021-10-12 00:54:40 +0300

    Assignment 7: Practicing Reactive Forms - create form (#14)

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  projectForm: FormGroup;
+
+  projectStatuses: string[] = ['Stable', 'Critical', 'Finished'];
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm() {
+
+    this.projectForm = this.formBuilder.group({
+      projectName: this.formBuilder.control(null, Validators.required),
+      email: this.formBuilder.control(null, [Validators.required, Validators.email]),
+      projectStatus: this.formBuilder.control(this.projectStatuses[0])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
