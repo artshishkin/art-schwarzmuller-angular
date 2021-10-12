8da4a9341b0701c7d03357b3846a8df9dacb9217
Art <d.art.shishkin@gmail.com>
2021-10-12 23:13:42 +0300

    226. Creating the Form For Editing Recipes (#14)

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -9,7 +9,7 @@ import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {CommonModule} from "@angular/common";
@@ -35,6 +35,7 @@ import {RecipeService} from "./recipes/recipe.service";
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     AppRoutingModule
   ],
--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Recipe} from "../recipe.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -13,16 +15,32 @@ export class RecipeEditComponent implements OnInit {
   id: number | null = null;
   recipe: Recipe;
 
-  constructor(private route: ActivatedRoute) {
+  recipeForm: FormGroup;
+
+  constructor(private route: ActivatedRoute,
+              private formBuilder: FormBuilder,
+              private recipeService: RecipeService) {
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
       this.editMode = paramMap.has('id');
       this.id = this.editMode ? +paramMap.get('id') : null;
+      this.recipe = this.editMode ?
+        this.recipeService.getRecipe(this.id) :
+        new Recipe('', '', '', []);
+      this.initForm();
     });
 
     this.route.data.subscribe(data => this.recipe = data['recipe']);
   }
 
+  private initForm() {
+    this.recipeForm = this.formBuilder.group({
+      name: this.formBuilder.control(this.recipe.name),
+      imagePath: this.formBuilder.control(this.recipe.imagePath),
+      description: this.formBuilder.control(this.recipe.description)
+    });
+    console.log(this.recipeForm);
+  }
 }
