aca674904146af8b4e0321506fa534795be46b56
Art <d.art.shishkin@gmail.com>
2021-10-16 10:18:14 +0300

    289. Handling Form Input (#19)

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
@@ -19,4 +20,8 @@ export class AuthComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
