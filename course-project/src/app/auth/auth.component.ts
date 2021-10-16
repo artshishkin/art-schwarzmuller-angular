483a09fc11ecc447467314aa2fc5b407b5593098
Art <d.art.shishkin@gmail.com>
2021-10-16 23:36:38 +0300

    293. Sending the Signup Request (#19)

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -14,14 +15,23 @@ export class AuthComponent implements OnInit {
     this.isLoginMode = !this.isLoginMode;
   }
 
-  constructor() {
+  constructor(private  authService: AuthService) {
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (form.invalid) return;
+    if (this.isLoginMode) {
+
+    } else {
+      this.authService.signUp(form.value.email, form.value.password)
+        .subscribe(
+          data => console.log(data),
+          error => console.log(error)
+        );
+    }
     form.reset();
   }
 }
