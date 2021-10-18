6ff984698e61ff9a5277117ac61cff073d8a3f8d
Art <d.art.shishkin@gmail.com>
2021-10-18 12:16:48 +0300

    309. Adding an Alert Modal Component (#20 Section 21: Dynamic Components)

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -21,6 +21,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptor} from "./auth/auth.interceptor";
+import {AlertComponent} from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import {AuthInterceptor} from "./auth/auth.interceptor";
     RecipeNotSelectedComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
