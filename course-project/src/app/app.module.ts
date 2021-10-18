c7d70990b41cbb0a52fa520040d99c7a51510515
Art <d.art.shishkin@gmail.com>
2021-10-18 09:09:19 +0300

    301. Attaching the Token with an Interceptor (#19)

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -17,9 +17,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {RecipeNotSelectedComponent} from './recipes/recipe-not-selected/recipe-not-selected.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from "./recipes/recipe.service";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
+import {AuthInterceptor} from "./auth/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [ShoppingListService, RecipeService],
+  providers: [ShoppingListService, RecipeService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Recipe} from "../recipes/recipe.model";
 import {RecipeService} from "../recipes/recipe.service";
-import {exhaustMap, map, take, tap} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 import {Observable} from "rxjs";
 import {AuthService} from "../auth/auth.service";
 
@@ -32,18 +32,7 @@ export class DataStorageService {
 
   fetchRecipesObservable(): Observable<Recipe[]> {
 
-    return this.authService.user.pipe(
-      take(1),
-
-      exhaustMap(
-        user => {
-          const token = user?.token;
-          return this.http.get<Recipe[]>(this.recipesUrl, {
-            params: {auth: `${token}`}
-          });
-        }
-      ),
-
+    return this.http.get<Recipe[]>(this.recipesUrl).pipe(
       map(recipes => recipes
         .map(recipe => {
             return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
