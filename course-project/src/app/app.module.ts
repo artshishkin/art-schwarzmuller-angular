4a7a047fadf54c9abced24adc388b2956aedaf01
Art <d.art.shishkin@gmail.com>
2021-10-19 09:25:25 +0300

    326. Understanding Shared Modules (#21)

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -4,29 +4,23 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {CommonModule} from "@angular/common";
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from "./recipes/recipe.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from './auth/auth.component';
-import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptor} from "./auth/auth.interceptor";
 import {AlertComponent} from './shared/alert/alert.component';
-import {PlaceholderDirective} from './shared/placeholder.directive';
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -36,7 +30,8 @@ import {ShoppingListModule} from "./shopping-list/shopping-list.module";
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService,
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
--- course-project/src/app/recipes/recipes.module.ts
+++ course-project/src/app/recipes/recipes.module.ts
@@ -1,5 +1,4 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 
 import {RecipeListComponent} from "./recipe-list/recipe-list.component";
 import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
@@ -9,6 +8,7 @@ import {RecipeNotSelectedComponent} from "./recipe-not-selected/recipe-not-selec
 import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {RecipesRoutingModule} from "./recipes-routing.module";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -20,9 +20,9 @@ import {RecipesRoutingModule} from "./recipes-routing.module";
     RecipeEditComponent,
   ],
   imports: [
-    CommonModule,
     ReactiveFormsModule,
-    RecipesRoutingModule
+    RecipesRoutingModule,
+    SharedModule
   ]
 })
 export class RecipesModule {
