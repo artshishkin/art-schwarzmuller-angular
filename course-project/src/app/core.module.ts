77c91825763b850def8d3ee1949fcd8c67e834db
Art <d.art.shishkin@gmail.com>
2021-10-20 09:02:40 +0300

    334.3. Loading Services Differently - through providers in Eagerly loaded module (result is the same) (#21)

--- course-project/src/app/core.module.ts
+++ course-project/src/app/core.module.ts
@@ -4,12 +4,14 @@ import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {RecipeService} from "./recipes/recipe.service";
 import {AuthInterceptor} from "./auth/auth.interceptor";
+import {LoggingService} from "./logging.service";
 
 @NgModule({
   providers: [
     ShoppingListService,
     RecipeService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    LoggingService
   ]
 })
 export class CoreModule {
