95cb9040a33de19b81ef33347116a7fe570f0c93
Art <d.art.shishkin@gmail.com>
2021-10-20 15:33:58 +0300

    349. Setting Up the NgRx Store (#24)

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -8,6 +8,8 @@ import {HeaderComponent} from './header/header.component';
 import {AlertComponent} from './shared/alert/alert.component';
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
+import {StoreModule} from "@ngrx/store";
+import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import {CoreModule} from "./core.module";
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     SharedModule,
     CoreModule,
   ],
