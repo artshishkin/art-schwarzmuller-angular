04eebdf0126533ff029ab6c1e7e3b815897e1d9a
Art <d.art.shishkin@gmail.com>
2021-10-20 12:46:43 +0300

    #22 fix default or error pages when splitting into modules

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
     path: 'shopping-list',
     loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
   },
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
-  // {path: '**', redirectTo: '/recipes'},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: '**', redirectTo: '/recipes'},
 ];
 
 @NgModule({
