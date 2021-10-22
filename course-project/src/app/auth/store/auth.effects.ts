ca46cad4f24f2056d19c52b28c4072757e25a14c
Art <d.art.shishkin@gmail.com>
2021-10-22 22:18:35 +0300

    367. Effects & Error Handling (#24)

--- course-project/src/app/auth/store/auth.effects.ts
+++ course-project/src/app/auth/store/auth.effects.ts
@@ -1,12 +1,47 @@
-import {Actions, ofType} from '@ngrx/effects';
+import {Actions, Effect, ofType} from '@ngrx/effects';
+import {HttpClient} from "@angular/common/http";
+import {catchError, map, switchMap} from "rxjs/operators";
+
 import * as AuthActions from './auth.actions';
+import {environment} from "../../../environments/environment";
+import {of} from "rxjs";
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
 export class AuthEffects {
 
+  @Effect()
   authLogin = this.actions$.pipe(
     ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      const requestBody = {
+        email: authData.payload.email,
+        password: authData.payload.password,
+        returnSecureToken: true
+      };
+      return this.http.post<AuthResponseData>(environment.loginUrl, requestBody)
+        .pipe(
+          catchError(error => {
+            //...
+            of()
+          }),
+          map((authResponse: AuthResponseData) => {
+            of();
+          })
+        );
+    })
   );
 
-  constructor(private actions$: Actions) {
+  constructor(private actions$: Actions,
+              private http: HttpClient) {
   }
+
+
 }
