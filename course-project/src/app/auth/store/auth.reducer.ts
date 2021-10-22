9a02600981a389677add9e578c3cb28d844ec211
Art <d.art.shishkin@gmail.com>
2021-10-22 17:21:14 +0300

    364. And Important Note on Actions - Prefix Pattern (#24)

--- course-project/src/app/auth/store/auth.reducer.ts
+++ course-project/src/app/auth/store/auth.reducer.ts
@@ -11,6 +11,8 @@ const initialState: State = {
 
 export function authReducer(state = initialState, action: AuthActions.AuthActions) {
 
+  // console.log(state);
+
   switch (action.type) {
     case AuthActions.LOGIN:
       const user = new User(
