ad80fdf4a57c405a0e20fc2849318a38f74eae0b
Art <d.art.shishkin@gmail.com>
2021-11-01 16:37:05 +0200

    412. Using Keyframes for Animations (#28)

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {animate, state, style, transition, trigger} from "@angular/animations";
+import {animate, keyframes, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -49,6 +49,40 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       transition('void => *', [
         style({opacity: 0, transform: 'translateX(-100px)'}),
         animate(500)
+      ]),
+      transition('* => void',
+        animate(500, style({opacity: 0, transform: 'translateX(100px)'}))
+      ),
+    ]),
+
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            opacity: 0,
+            transform: 'translateX(-100px)',
+            offset: 0
+          }),
+          style({
+            opacity: 0.5,
+            transform: 'translateX(-50px)',
+            offset: 0.3
+          }),
+          style({
+            opacity: 1,
+            transform: 'translateX(-20px)',
+            offset: 0.8
+          }),
+          style({
+            opacity: 1,
+            transform: 'translateX(0px)',
+            offset: 1
+          }),
+        ]))
       ]),
       transition('* => void',
         animate(500, style({opacity: 0, transform: 'translateX(100px)'}))
