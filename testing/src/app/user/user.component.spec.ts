5f89506435c829b02fa3f9118489786ea8e1b3a0
Art <d.art.shishkin@gmail.com>
2021-11-05 10:47:36 +0200

    428. Simulating Async Tasks (#30)

--- testing/src/app/user/user.component.spec.ts
+++ testing/src/app/user/user.component.spec.ts
@@ -1,7 +1,8 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {UserComponent} from './user.component';
 import {UserService} from "./user.service";
+import {DataService} from "../shared/data.service";
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -13,35 +14,59 @@ describe('UserComponent', () => {
     })
       .compileComponents();
   });
+  describe('using before each', () => {
 
-  beforeEach(() => {
+    beforeEach(() => {
+      fixture = TestBed.createComponent(UserComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should use username from the service', () => {
+      const userService = fixture.debugElement.injector.get(UserService);
+      expect(component.user.name).toEqual(userService.user.name);
+    });
+
+    it('should display the username if the user is logged in', function () {
+      component.isLoggedIn = true;
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('p').textContent).toEqual('User is: Kate');
+    });
+
+    it("shouldn't display the username if the user is not logged in", function () {
+      component.isLoggedIn = false;
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('p').textContent).not.toContain('User is:');
+    });
+
+    it('should not fetch data successfully if not called asynchronously', function () {
+      const dataService = fixture.debugElement.injector.get(DataService);
+      const spy = spyOn(dataService, "getDetails")
+        .and.returnValue(Promise.resolve("Dummy Data"));
+      fixture.detectChanges();
+      expect(component.data).toBe(undefined);
+    });
+  });
+
+  it('should fetch data successfully if called asynchronously', waitForAsync(() => {
+
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    const dataService = fixture.debugElement.injector.get(DataService);
+
+    const spy = spyOn(dataService, "getDetails")
+      .and.returnValue(Promise.resolve("Dummy Data"));
+
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should use username from the service', () => {
-    const userService = fixture.debugElement.injector.get(UserService);
-    expect(component.user.name).toEqual(userService.user.name);
-  });
-
-  it('should display the username if the user is logged in', function () {
-    component.isLoggedIn = true;
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('p')?.textContent).toEqual('User is: Kate');
-  });
-
-  it("shouldn't display the username if the user is not logged in", function () {
-    component.isLoggedIn = false;
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('p')?.textContent).not.toContain('User is:');
-  });
-
+    fixture.whenStable().then(() => {
+      expect(component.data).toBe("Dummy Data");
+    });
+  }));
 
 });
--- testing/src/app/user/user.component.ts
+++ testing/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "./user.service";
+import {DataService} from "../shared/data.service";
 
 @Component({
   selector: 'app-user',
@@ -9,12 +10,14 @@ import {UserService} from "./user.service";
 export class UserComponent implements OnInit {
   user: { name: string } = {name: 'art'};
   isLoggedIn = false;
+  data: string;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private dataService: DataService) {
   }
 
   ngOnInit(): void {
     this.user = this.userService.user;
+    this.dataService.getDetails().then(data => this.data = data);
   }
 
 }
