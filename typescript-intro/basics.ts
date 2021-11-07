ccd6b02db0cc0bfad8263f8f72f5f05ce70062e1
Art <d.art.shishkin@gmail.com>
2021-11-07 20:25:05 +0200

    454. Array & Object Types (#33)

--- typescript-intro/basics.ts
+++ typescript-intro/basics.ts
@@ -16,5 +16,28 @@ let isInstructor: boolean;
 
 isInstructor = true;
 
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Max',
+    age: 32,
+};
+
+let people: {
+    name: string;
+    age: number;
+}[];
+
+
+
 
 
